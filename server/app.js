7697d7df67b3092bf6d95e01756ab38090f0fe8c
FatimaSami <84532632+fatima-Sami55@users.noreply.github.com>
2026-05-16 16:04:56 +0500

    updated app.js

--- server/app.js
+++ server/app.js
@@ -30,7 +30,7 @@ mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
     console.log('✅ MongoDB connected');
-    const PORT = process.env.PORT || 5000;
+    const PORT = process.env.PORT || 8000;
     app.listen(PORT, () => {
       console.log(`🔐 CipherVault server running on port ${PORT}`);
     });
